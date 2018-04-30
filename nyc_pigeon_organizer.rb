905eb5e3cf9e5ebb8ba245164f3adc44cdeee3a5
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-30 01:10:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  {}
+  {
+      "Theo": nil,
+      "Peter Jr.": nil,
+      "Lucky": nil,
+      "Ms. K": nil,
+      "Queenie": nil,
+      "Andrew": nil,
+      "Alex": nil
+  }
 end
