e079ae006942775e58eab40c93fc0ad13a1a85e4
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-30 02:01:58 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,15 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   names = get_pigeons_names(data)
-  data
+  
 end
 
 def get_pigeons_names(data)
+    names = []
+    data.each do |key, value|
+        value.each do |key, value|
+            names.push(value)
+        end
+    end
+    names.flatten,. uniq
 end
