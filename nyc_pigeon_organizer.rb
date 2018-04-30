7a007066050b06ce14ea0790853964fdf6a31597
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-30 01:56:07 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,16 +1,6 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  {
-      "Theo" => {
-          color: ["purple", "grey"],
-          gender: ["male"],
-          lives: ["Subway"]
-      },
-      "Peter Jr." => nil,
-      "Lucky" => nil,
-      "Ms. K" => nil,
-      "Queenie" => nil,
-      "Andrew" => nil,
-      "Alex" => nil
-  }
+  new_hash = {}
+  
+  data
 end
