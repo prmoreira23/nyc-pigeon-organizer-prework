a706903b4fae176726882ed34a40ce90df80d21d
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-30 01:39:41 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
       "Theo" => {
           color: ["purple", "grey"],
           gender: "male",
-          lives: 
+          lives: "subway"
       },
       "Peter Jr." => nil,
       "Lucky" => nil,
